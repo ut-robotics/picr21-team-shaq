7f587d9a54082e10bd345887bc43ca20160b03a9
Nikita Naumov <niknau2009@gmail.com>
2022-01-19 15:18:08 +0200

    ä

--- src/client.py
+++ src/client.py
@@ -4,27 +4,29 @@ import ast
 
 class Client:
 
-	def __init__(self, ipaddr, port):
-		asyncio.run(hello(ipaddr, port))
+	def __init__(self, ipaddr, portnum):
+		self.received_info = None
+		asyncio.run(self.hello(ipaddr, portnum))
 
-async def hello(ipaddr, portnum):
+	async def hello(self, ipaddr, portnum):
 
-	async for websocket in websockets.connect("ws://" + ipaddr + ":" + portnum):
-		try:
-			received = await websocket.recv()
-			print(received)
+		async for websocket in websockets.connect("ws://" + ipaddr + ":" + portnum):
 			try:
-				received_dict = ast.literal_eval(received)
-				#print(received_dict)
-				#print(type(received_dict))
-			except ValueError:
-				pass
+				received = await websocket.recv()
+				print(received)
+				try:
+					received_dict = ast.literal_eval(received)
+					#print(received_dict)
+					#print(type(received_dict))
+					self.received_info = received_dict
+				except ValueError:
+					pass
 
-			#toserver = "just in case"
-			#await websocket.send(toserver)
+				#toserver = "just in case"
+				#await websocket.send(toserver)
 
-		except websockets.ConnectionClosed:
-			continue
+			except websockets.ConnectionClosed:
+				continue
 
 if __name__ == "__main__":
 	socketinfo_str = input("enter socket address and port: ")
@@ -36,4 +38,4 @@ if __name__ == "__main__":
 	#uri = "ws://localhost:8765"
 	#uri = "ws://192.168.3.98:8765"
 
-	asyncio.run(hello(socketinfo[0], socketinfo[1]))
+	Client(socketinfo[0], socketinfo[1])
