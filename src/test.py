29e446dd37a5683ec4ffdd2c061b877426d5b12b
Nikita Naumov <niknau2009@gmail.com>
2022-01-20 16:36:41 +0200

    now getting info from client using queue

--- src/test.py
+++ src/test.py
@@ -1,63 +1,22 @@
 from client import Client
 import time
 from threading import Thread
+import queue
 
 ipaddr = "192.168.3.98"
 #ipaddr = "localhost"
 portnum = "8765"
 
-class WebsocketThread(Thread):
-	def __init__(self, ipaddr, portnum):
-		super().__init__()
-		self._target = Client
-		self._args = (ipaddr, portnum)
-		#self.client = Client(ipaddr, portnum)
-		#self.client = self._target(self._args[0], self._args[1])
-		#self.last_event = None
-		#self.running = False
-		self.client = None
-
-	def run(self):
-		self.running = True
-		try:
-			if self._target:
-				self.client = self._target(*self._args, **self._kwargs)
-		finally:
-			del self._target, self._args, self._kwargs
-			#self.client = None
-		#while self.running:
-			#do some websocket stuff
-			#self.last_event = "something happened?" # something that you want
-
-	#def event_processed(self):
-		#self.last_event = None
-
-thread = WebsocketThread(ipaddr, portnum)
+recv_queue = queue.Queue()
+thread = Thread(target=Client, args=(ipaddr, portnum, recv_queue))
 thread.start()
 
 # main loop
 while True:
-	#if thread.last_event is not None:
-		#handle some stuff
-		#print(thread.last_event)
-		#thread.event_processed()
-
-	# main game logic
 	print("a")
-	time.sleep(3)
-	if thread.client != None:
-		if thread.client.received_info != None:
-			print(thread.client.received_info)
-		else:
-			print("no info :(")
-	else:
-		print("no client :(")
-
-##testthread = threading.Thread(target=Client, args=(ipaddr, portnum))
-##testthread.start()
-#client_obj = Client(ipaddr, portnum)
-
-#while True:
-	#print("a")
-	#time.sleep(3)
-	##print(testthread.local().received_info)
+	time.sleep(0.5)
+	try:
+		received_data = recv_queue.get(block=False)
+		print(received_data)
+	except queue.Empty:
+		pass
